ce39facfcbce090f5dabe5ebb31f904430230174
Laszlo Fent <Fent@MacBook-Pro-de-MacOs.local>
2017-10-22 02:33:44 -0200

    Ajustes no cálculo total da compra - Rodrigo

--- LuanaRodrigo/TotalViewController.swift
+++ LuanaRodrigo/TotalViewController.swift
@@ -41,6 +41,57 @@ class TotalViewController: UIViewController {
         let iof = Double(UserDefaults.standard.string(forKey: "iof") ?? "6.38")!
         
         var totDolar: Double = 0.0
+        var totDolarPlusTax: Double = 0.0
+        var iofProd: Double = 0.0
+        var taxProd: Double = 0.0
+        var totReal: Double = 0.0
+        
+        for product in product {
+            
+            // total em dolar a exibir.
+            totDolar += product.value
+            
+            // calcula taxa do estado para o produto
+            taxProd = (product.value * (Double((product.states?.tax)!) / 100))
+            
+            // verifica se é cartão para acrescentar taxa IOF.
+            if product.cardPayment {
+                iofProd = (product.value * (iof / 100))
+            }
+            
+            // faz a soma do valor do produto + taxas.
+            totDolarPlusTax += (product.value + taxProd + iofProd)
+            
+            print("log")
+            print("STATE: ", product.states?.name! ?? "Nenhum")
+            print("TAX State: \(Double((product.states?.tax)!))")
+            
+            print("VAL U$$: \(product.value)")
+            print("Cartão: \(product.cardPayment)")
+            print("IOF PROD: \(iofProd)")
+            print("TAX PROD: \(taxProd)")
+            print("U$$+TAX+IOF: \((product.value + taxProd + iofProd))")
+            
+            print("SOMAComTAX: \(totDolarPlusTax)")
+
+            // resseta variaveis de IOF e Tax para calculo do próximo produto
+            iofProd = 0.0
+            taxProd = 0.0
+        }
+
+        // calcula total em reais (valor de todos os produtos e respectivas taxas * cotação do dólar).
+        totReal = totDolarPlusTax * cotacaoD
+
+        // formata totais com duas casas decimais.
+        tfTotDolar.text = String(format: "%.2f", totDolar)
+        tfTotReal.text = String(format: "%.2f", totReal)
+    }
+
+    func calcularObsolete(){
+        let cotacaoD = Double(UserDefaults.standard.string(forKey: "dolar") ?? "3.2")!
+        let iof = Double(UserDefaults.standard.string(forKey: "iof") ?? "6.38")!
+        
+        var totDolar: Double = 0.0
         var totReal: Double = 0.0
         var sumReal: Double = 0.0
         
